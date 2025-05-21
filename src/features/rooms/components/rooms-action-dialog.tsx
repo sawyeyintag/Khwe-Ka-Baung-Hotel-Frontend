669f98f679e4da409403d975060d6aa31e0c7a8d
Saw Ye Yint Aung <5352sawyeyintaung@gmail.com>
2025-05-21 23:09:18 +0700

    feat: Implement RoomsActionDialog and UsersDialogs components for managing rooms

---
+++ src/features/rooms/components/rooms-action-dialog.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { showSubmittedData } from "@/utils/show-submitted-data";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SelectDropdown } from "@/components/select-dropdown";
+import { Room } from "../schema/schema";
+
+const formSchema = z.object({
+  roomNumber: z.number().refine((value) => value >= 100 && value <= 999, {
+    message: "Room number must be a 3-digit number",
+  }),
+  floorNumber: z.number(),
+  roomTypeId: z.number(),
+  isEdit: z.boolean(),
+});
+
+type RoomForm = z.infer<typeof formSchema>;
+
+interface Props {
+  currentRow?: Room;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function RoomsActionDialog({ currentRow, open, onOpenChange }: Props) {
+  const isEdit = !!currentRow;
+  const form = useForm<RoomForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: isEdit
+      ? {
+          ...currentRow,
+          isEdit,
+        }
+      : {
+          roomNumber: 0,
+          floorNumber: 0,
+          roomTypeId: 0,
+        },
+  });
+
+  const onSubmit = (values: RoomForm) => {
+    form.reset();
+    showSubmittedData(values);
+    onOpenChange(false);
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={(state) => {
+        form.reset();
+        onOpenChange(state);
+      }}
+    >
+      <DialogContent className='sm:max-w-lg'>
+        <DialogHeader className='text-left'>
+          <DialogTitle>{isEdit ? "Edit Room" : "Add New Room"}</DialogTitle>
+          <DialogDescription>
+            {isEdit ? "Update the room here. " : "Create new room here. "}
+            Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className='-mr-4 h-[26.25rem] w-full overflow-y-auto py-1 pr-4'>
+          <Form {...form}>
+            <form
+              id='room-form'
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='space-y-4 p-0.5'
+            >
+              <FormField
+                control={form.control}
+                name='roomNumber'
+                render={({ field }) => (
+                  <FormItem className='grid grid-cols-6 items-center space-y-0 gap-x-4 gap-y-1'>
+                    <FormLabel className='col-span-2 text-right'>
+                      Room Number
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='101'
+                        className='col-span-4'
+                        type='number'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className='col-span-4 col-start-3' />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='floorNumber'
+                render={({ field }) => (
+                  <FormItem className='grid grid-cols-6 items-center space-y-0 gap-x-4 gap-y-1'>
+                    <FormLabel className='col-span-2 text-right'>
+                      Floor Number
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='1'
+                        className='col-span-4'
+                        type='number'
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage className='col-span-4 col-start-3' />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+        <DialogFooter>
+          <Button type='submit' form='room-form'>
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
